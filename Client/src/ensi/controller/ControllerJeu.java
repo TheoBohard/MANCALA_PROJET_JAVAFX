37c6ec07e97b5e0a4b50e50226507cf95a3e6fb7
Théo BOHARD <theobohard00@gmail.com>
2021-06-18 15:21:39 +0200

    [Feature] Rules display added
    Signed-off-by: Théo BOHARD <theobohard00@gmail.com>

--- Client/src/ensi/controller/ControllerJeu.java
+++ Client/src/ensi/controller/ControllerJeu.java
@@ -15,10 +15,8 @@ import javafx.scene.media.MediaPlayer;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.text.Text;
-import javafx.stage.Modality;
 import javafx.stage.PopupWindow;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.File;
 import java.io.IOException;
@@ -320,9 +318,8 @@ public class ControllerJeu implements Initializable {
 
             if(response instanceof ArrayList) {
                 updateView((ArrayList<?>) response);
-            }
-            else if (response instanceof String) {
-                if(response == "Deplacement impossible") {
+            } else if (response instanceof String) {
+                if (response == "Deplacement impossible") {
                     System.out.println("Ce n'est pas possible");
                 }
             }
@@ -330,10 +327,43 @@ public class ControllerJeu implements Initializable {
         });
 
         about.setOnAction(e -> displayAbout());
+
+        seeRules.setOnAction(e -> displayRules());
+    }
+
+    /**
+     * This function permit to display the rules
+     */
+    void displayRules() {
+        String msg = "Règle 1 : Seulement deux joueurs peuvent s'affronter.\n" +
+                "\n" +
+                "Règle 2 : On répartit quarante-huit graines dans les douze trous à raison de quatre graines par trou.\n" +
+                "\n" +
+                "Règle 3 : Chaque joueur joue à tour de rôle, celui qui joue en premier est tiré au hasard. Le joueur va prendre l'ensemble des graines dans l'un des trous de son territoire et les distribuer, un par trou, dans le sens inverse des aiguilles d'une montre.\n" +
+                "\n" +
+                "Règle 4 : Si sa dernière graine tombe dans un trou du camp adverse et qu'il y a maintenant deux ou trois graines dans ce trou, le joueur récupère ces deux ou trois graines et les met de côté. Ensuite il regarde la case précédente : si elle est dans le camp adverse et contient deux ou trois graines, il récupère ces graines, et ainsi de suite jusqu'à ce qu'il arrive à son camp ou jusqu'à ce qu'il y ait un nombre de graines différent de deux ou trois.\n" +
+                "\n" +
+                "Règle 5 : On ne saute pas de case lorsqu'on égrène sauf lorsqu'on a douze graines ou plus, c'est-à-dire qu'on fait un tour complet : on ne remplit pas la case dans laquelle on vient de prendre les graines.\n" +
+                "\n" +
+                "Règle 6 : Il faut « nourrir » l'adversaire, c'est-à-dire que, quand celui-ci n'a plus de graines, il faut absolument jouer un coup qui lui permette de rejouer ensuite. Si ce n'est pas possible, la partie s'arrête et le joueur qui allait jouer capture les graines restantes.\n" +
+                "\n" +
+                "Règle 7 : Si un coup devait prendre toutes les graines adverses, alors le coup peut être joué, mais aucune capture n'est faite : il ne faut pas « affamer » l'adversaire.\n" +
+                "\n" +
+                "Règle 8 : La partie s'arrête quand : ➢ Un des joueurs a capturé au moins 25 graines, et est désigné gagnant (mode moyen) ➢ Ou qu'il ne reste qu'au plus 6 graines en jeu et que l’un des joueurs n’a plus de graines dans son camp et que son adversaire n’a pas eu l’opportunité de lui en redonner (mode débutant)\n" +
+                "\n" +
+                "Règle 9 : Quand il ne reste qu'au plus 10 graines sur le plateau, le joueur qui a la main peut proposer l'abandon de la partie. Si il est accepté les deux joueurs se partagent les graines restantes. Si le total des graines du plateau est inférieur à 6, sans qu'aucun des joueurs n'a un total de graines supérieur à 24. La partie est nulle.";
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.getDialogPane().setMinSize(600, 700);
+        alert.getDialogPane().setContentText(msg);
+        alert.getDialogPane().setHeaderText("Règles ");
+        alert.setTitle("Règles ");
+        alert.show();
     }
 
     /**
      * This function permit to ask to the use to surrender or not
+     *
      * @param message The message we want to display
      * @return If he want to surrender
      */
@@ -356,8 +386,6 @@ public class ControllerJeu implements Initializable {
     private void displayAbout() {
         String msg = "Mancala - Copyright 2021 \nRéalisé par Théo BOHARD & Thomas FILLION \nENSICAEN";
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.initStyle(StageStyle.UNIFIED);
-        alert.initModality(Modality.WINDOW_MODAL);
         alert.getDialogPane().setContentText(msg);
         alert.getDialogPane().setHeaderText("A propos");
         alert.setTitle("A propos");
